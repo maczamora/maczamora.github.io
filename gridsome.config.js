2f51ac9e3ee22dd0032928f05a02aef852fe804e
Mario zamora <mario.zamora@keysight.com>
2020-09-04 19:18:36 -0600

    Testing with gh-pages

--- gridsome.config.js
+++ gridsome.config.js
@@ -6,7 +6,8 @@
 
 module.exports = {
   siteName: 'Cyber Cowboy',
-  siteUrl: 'https://maczamora>.github.io',
+  siteUrl: 'https://maczamora.github.io',
+  pathPrefix: '/maczamora.github.io',
   siteDescription: 'Dedicated blog to deliver content surrounding DevOps, DevSecOps, Cyberecurity and anything and everything around Linux',
   plugins: []
 }
