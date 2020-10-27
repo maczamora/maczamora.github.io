f9a21cc9d08c4d032c9a8448ce3acf1437e3ec3a
Mario zamora <mario.zamora@keysight.com>
2020-10-26 19:08:17 -0600

    Pusing content changes

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -6,17 +6,37 @@
   </div>
 
   <div class="content-container">
-    <p class="about-info">
+    <h4 class="career">Career Background</h4>
+    <p class="career-info">
      I'm a CIS (Computer Information Systems) under-graduate from CSU-Pueblo 🐺 with an emphais in Cybersecurity.
+     During my time in college I worked as a Helpdesk Support Technician for a company called <a href="https://www.surfboard.com/">ARRIS</a>, and as a Network
+     Admin Assistant for the Network and Server Systems department on campus. I got to participate in a campus network upgrade where I installed Wi-Fi Access Points and Switches suppported by 
+     a company called <a href="https://www.extremenetworks.com/">Extreme Networks.</a> You can say that I had my fair share and time in the trenches. After graduation I was blessed to have 
+     received a job offer from <a href="https://www.extremenetworks.com/">Keysight Technologies</a> as an IT Associate part of the first and last IT Rapid Rotation Program.
     </p>
-    <p class="about-info">
-      I am looking to start contributing more to the open-source space, starting new personal projects (hacker status), build and lead a business (aspiring entrepreneur) and growing both in my technical and business confidence.
-    </p>
-    <p class="about-info">
-      Please feel free to reach out to me via email if you would like to collaborate on a project or if you have any questions and need some consultative work. If not go ahead and scroll to the bottom and give the Twitter a follow or reach out to me via the social sites below.
-    </p>
+     <p class="career-info"> 
+      During this program I rotated between different areas within IT including Security Operations, Compliance, Infrastructure and Customer Applications. The initial plan of the program
+      was for those in the cohort to go they're separate ways into a specific area of interest with the IT organization. Those plans were changes, as the 2019 cohort and 2018 cohort were 
+      asked to create the IT Agile Center of Excellence and deliver valueable software products using Agile practices.
+     </p>
+     <p class="career-info"> 
+       It has been a journey as I've transtioned more into a software role and it has been both challenging and rewarding. I've gained a set of new skills and keep growing and learning every day.
+       I've gotten to work on both successful and not so successful software products and services and learn the ins and outs of working on a Agile Software Team. 
+     </p>
+     <h4 class="career">My Life</h4>
+      <p class="career-info">
+        Outside my career I value family and friends. I'm married to my Wife Elizabeth and have a Dog named Yoda. He's the best and very spoiled. I work hard every day to provide for my family as 
+        I want to provide an even better future my kid(s). I enjoy getting outside here in beatufiul Colorado, exercise at the gym, reading up new concepts and theory, making money and best of all 
+        finding my hustle. 
+      </p>
+      <p class="career-info">
+        I enjoy all kinds of music, versatile in my clothing options, and starting a new blog and website to share ideas, thoughts and content. 
+      </p>
+     <h4 class="career">Road Ahead</h4>
+      <p class="career-info">
+        I am looking to start contributing more to the open-source space, starting new personal projects (hacker status), build and lead a business (aspiring entrepreneur) and growing both in my technical and business confidence.
+      </p>
   </div>
-  
   <div class="bottom-container">
     <section class="contact-info">
       Please feel free to reach out to me via email if you would like to collaborate on a project or if you have any questions and need some consultative work. If not go ahead and scroll to the bottom and give the Twitter a follow or reach out to me via the social sites below.
