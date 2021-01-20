26e911bd0d04cca1c53d0b75e482ee08999f58d7
Mario zamora <mario.zamora@keysight.com>
2021-01-19 18:38:25 -0700

    Refactor: completed formatting for home page!

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -5,10 +5,12 @@
       <!-- <g-image alt="Example image" src="~/favicon.png" width="135" /> -->
       <!-- Use gopher image in the Header-->
     </header-layout>
-    <div class="jumbotron">
+    <div class="jumbotron border border-top-0 border-white">
       <b-jumbotron header="Engineered for Hustle" lead="Full time IT Software Engineer working on Smarter-IT initiaitives,
         automation and building scalable, reliable, bug free software at
         Keysight Technologies.">
+        <hr>
+        <hr>
         <p>
           For more information or if you would like to contact me please submit
           a message from the contact page. Please feel free to explore my personal website and blog and hope you
@@ -17,7 +19,7 @@
       </b-jumbotron>
     </div>
     <b-container class="main-container">
-      <b-row class="pb-5">
+      <b-row class="pb-5 pt-5">
         <b-col></b-col>
         <b-col cols="10">As you begin your journey into my website and personal blog I hope you find your time valueable.</b-col>
         <b-col></b-col>
@@ -48,9 +50,9 @@
         </b-col>
         <b-col></b-col>
       </b-row>
-      <b-row class="pb-5 pt-5">
+      <b-row class="pb-1 pt-5">
         <b-col></b-col>
-        <b-col cols="10">Image Here!</b-col>
+        <b-col cols="12"><b-img-lazy v-bing="imageProps" rounded src="safar-safarov-LKsHwgzyk7c-unsplash.jpg" fluid alt="Responsive image"></b-img-lazy></b-col>
         <b-col></b-col>
       </b-row>
     </b-container>
@@ -63,7 +65,12 @@ export default {
   metaInfo: {
     title: "Mario Zamora",
   },
-};
+    data() {
+      return {
+        imageProps: {}
+      }
+    }
+  };
 </script>
 
 <style>
